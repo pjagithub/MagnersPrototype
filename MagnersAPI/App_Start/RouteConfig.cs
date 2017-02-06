e502bcece9a277288e3333d3f761ee0924788663
pjagithub <paul.attridge@gmail.com>
2017-02-06 14:04:16 -0700

    Add files via upload

---
+++ MagnersAPI/App_Start/RouteConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Magners
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+            // The following route depends on the installation of a NuGet package to 
+            // provide a JSONP (JSON padded with callback function) formatter for Web API
+            //  Install-Package WebApiContrib.Formatting.Jsonp
+            // http://www.codeproject.com/Tips/631685/JSONP-in-ASP-NET-Web-API-Quick-Get-Started
+            // 
+            routes.MapHttpRoute(
+                name: "DefaultJsonpApi",
+                routeTemplate: "{controller}/{action}/{id}/{format}",
+                defaults: new { id = RouteParameter.Optional, format = RouteParameter.Optional }
+            );
+        }
+    }
+}
